174f1053edc5da3c6e4700cabbd2ada836169bfd
damir1-cloud <66561971+damir1-cloud@users.noreply.github.com>
2020-08-07 03:10:49 +0500

    destructing and undefined error removed

--- src/API/index.js
+++ src/API/index.js
@@ -2,13 +2,23 @@
  export const fetchData = async ()=>{
     const response = await fetch ('https://covid19.mathdro.id/api');
 
-const data = await response.json();
+const {confirmed, recovered, deaths, lastUpdate} = await response.json();
      
- const modifiedData = {
+ /*const modifiedData = {
     confirmed: data && data.confirmed && data.confirmed.value,
     recovered: data && data.recovered && data.recovered.value,
     deaths: data && data.deaths && data.deaths.value,
     lastUpdate: data && data.lastUpdate,
+}*/
+
+
+
+
+const modifiedData = {
+    confirmed: confirmed,
+    recovered: recovered,
+    deaths: deaths,
+    lastUpdate: lastUpdate,
 }
 return (modifiedData);
 }
