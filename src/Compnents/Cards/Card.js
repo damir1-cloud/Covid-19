d6369742c2275b121c8f11c080ed57fe11d96134
damir1-cloud <66561971+damir1-cloud@users.noreply.github.com>
2020-08-08 03:18:27 +0500

    CountUP
    countup from react-countup anmation added to numbers

--- src/Compnents/Cards/Card.js
+++ src/Compnents/Cards/Card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 import styles from "./card.module.css";
+import CountUp from 'react-countup';
 
  const Cards = ({data : {confirmed, recovered, deaths, }})=> {
      //console.log();
@@ -11,20 +12,20 @@ import styles from "./card.module.css";
         <div className = {styles.container}>
            <Grid container spacing={3} justify="center">
                 <Grid item component={Card}>
-                    <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                    <Typography variant="h5">{confirmed.value}</Typography>
+                    <Typography color="textSecondary" gutterBottom>Confirmed</Typography>
+                    <Typography variant="h5"><CountUp start = {0} end = {confirmed.value} duration = {2} separator = ","/></Typography>
                     <Typography color="textSecondary">date</Typography>
                     <Typography variant="body2">active cases of covid</Typography>
                 </Grid>
                 <Grid item component={Card}>
                     <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-                    <Typography variant="h5">{recovered.value}</Typography>
+                    <Typography variant="h5"><CountUp start = {0} end = {recovered.value} duration = {2} separator = ","/></Typography>
                     <Typography color="textSecondary">date</Typography>
                     <Typography variant="body2">recovered cases of covid</Typography>
                 </Grid>
                 <Grid item component={Card}>
                     <Typography color="textSecondary" gutterBottom>Deaths</Typography>
-                    <Typography variant="h5">{deaths.value}</Typography>
+                    <Typography variant="h5"><CountUp start = {0} end = {deaths.value} duration = {2} separator = ","/></Typography>
                     <Typography color="textSecondary">date</Typography>
                     <Typography variant="body2">deaths cases of covid</Typography>
                 </Grid>
